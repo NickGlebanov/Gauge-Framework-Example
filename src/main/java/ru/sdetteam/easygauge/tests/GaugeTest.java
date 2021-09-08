c604e672d5557bc1d3d4ad43c505299291d59aba
ZmixZ <60565565+ZmixZ@users.noreply.github.com>
2021-09-08 20:48:45 +0300

    first

---
+++ src/main/java/ru/sdetteam/easygauge/tests/GaugeTest.java
@@ -0,0 +1,44 @@
+package ru.sdetteam.easygauge.tests;
+
+import com.codeborne.selenide.Browsers;
+import com.codeborne.selenide.Configuration;
+import com.thoughtworks.gauge.Step;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GaugeTest {
+    @BeforeClass()
+    public void setUp() {
+        WebDriverManager.firefoxdriver().setup();
+        Configuration.browser = Browsers.CHROME;
+        Configuration.timeout = 20000;
+    }
+
+
+    @Test
+    public void testGauge() {
+        openURL();
+        writeLogin("administration", "root");
+    }
+
+    @Step("Goto mantis page")
+    public void openURL (){
+        open("http://192.168.0.38");
+    }
+
+    @Step("Write login data <login> <pass>")
+    public void writeLogin(final String login, final String pass){
+        $(byId("username")).setValue(login);
+        $(byText("Войти")).click();
+        $(byId("password")).setValue(pass);
+        $(byText("Войти")).click();
+    }
+}
---
+++ src/main/java/ru/sdetteam/easygauge/tests/gauge.spec
@@ -0,0 +1,8 @@
+# Login on mantis
+Tags: login
+
+## Open and login mantis
+
+* Goto mantis page
+
+* Write login data "administration" "root"
